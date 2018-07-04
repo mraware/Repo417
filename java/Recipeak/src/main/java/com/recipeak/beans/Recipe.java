2fd6e5d8047650aa9c2631953a1f801369b1f547
Jacob Miller <millerjacob67@gmail.com>
2018-07-04 19:16:07 -0400

    Minor improvements.

--- java/Recipeak/src/main/java/com/recipeak/beans/Recipe.java
+++ java/Recipeak/src/main/java/com/recipeak/beans/Recipe.java
@@ -5,6 +5,68 @@ import javax.persistence.Entity;
 @Entity
 public class Recipe 
 {
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + burns;
+		result = prime * result + creator;
+		result = prime * result + flavor;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((notes == null) ? 0 : notes.hashCode());
+		result = prime * result + ((privacy == null) ? 0 : privacy.hashCode());
+		result = prime * result + promoted;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		if (burns != other.burns)
+			return false;
+		if (creator != other.creator)
+			return false;
+		if (flavor != other.flavor)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (notes == null) {
+			if (other.notes != null)
+				return false;
+		} else if (!notes.equals(other.notes))
+			return false;
+		if (privacy == null) {
+			if (other.privacy != null)
+				return false;
+		} else if (!privacy.equals(other.privacy))
+			return false;
+		if (promoted != other.promoted)
+			return false;
+		return true;
+	}
+	public Recipe(String name, int flavor, int creator, String privacy, int burns, int promoted, String notes) {
+		super();
+		this.name = name;
+		this.flavor = flavor;
+		this.creator = creator;
+		this.privacy = privacy;
+		this.burns = burns;
+		this.promoted = promoted;
+		this.notes = notes;
+	}
+	public Recipe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	String name;
 	int flavor;
 	int creator;
