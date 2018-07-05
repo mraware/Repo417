7e6bdaf2e17496091f9809eef91eb263ac4926bc
Aware14 <aaronware14@gmail.com>
2018-07-05 15:46:47 -0400

    HistoryDAO

---
+++ java/Recipeak/src/main/java/com/revature/beans/History.java
@@ -0,0 +1,138 @@
+package com.revature.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName="prototype")
+@Entity
+@Table(name="History")
+public class History {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="history")
+	@SequenceGenerator(name="history", sequenceName="history_seq", allocationSize=1)
+	@Column(name="History_id")
+	private int id;
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="user_id", insertable=false, updatable=false)
+	private User user;
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="recipe_id", insertable=false, updatable=false)
+	private Recipe recipe;
+	private int saved;
+	private int score;
+	private String review;
+	public History() {
+		super();
+	}
+	@Autowired
+	public History(int id, User user, Recipe recipe, int saved, int score, String review) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.recipe = recipe;
+		this.saved = saved;
+		this.score = score;
+		this.review = review;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((recipe == null) ? 0 : recipe.hashCode());
+		result = prime * result + ((review == null) ? 0 : review.hashCode());
+		result = prime * result + saved;
+		result = prime * result + score;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		History other = (History) obj;
+		if (id != other.id)
+			return false;
+		if (recipe == null) {
+			if (other.recipe != null)
+				return false;
+		} else if (!recipe.equals(other.recipe))
+			return false;
+		if (review == null) {
+			if (other.review != null)
+				return false;
+		} else if (!review.equals(other.review))
+			return false;
+		if (saved != other.saved)
+			return false;
+		if (score != other.score)
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "History [id=" + id + ", user=" + user + ", recipe=" + recipe + ", saved=" + saved + ", score=" + score
+				+ ", review=" + review + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public int getSaved() {
+		return saved;
+	}
+	public void setSaved(int saved) {
+		this.saved = saved;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	
+}
---
+++ java/Recipeak/src/main/java/com/revature/data/HistoryHibernateDAO.java
@@ -0,0 +1,52 @@
+package com.revature.data;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+
+
+import com.revature.beans.History;
+import com.revature.beans.User;
+
+@Component
+public class HistoryHibernateDAO implements HistoryDAO, HibernateSession {
+	private volatile Session session;
+
+	@Override
+	public History addHistory(History history) {
+		session.save(history);
+		return history;
+		
+	}
+
+	@Override
+	public History getHistoryById(int id) {
+		return (History) session.get(History.class, id);
+	}
+
+	@Override
+	public History updateHistory(History history) {
+		session.update(history);
+		return history;
+		
+	}
+
+	@Override
+	public void deleteHistory(History history) {
+		session.delete(history);
+		
+	}
+
+	@Override
+	public void setSession(Session session) {
+		this.session = session;
+		
+	}
+
+	@Override
+	public List<History> historyByUser(User user) {
+		return (List<History>) session.createQuery("From com.revature.beans.History", History.class).list();
+	}
+
+}
