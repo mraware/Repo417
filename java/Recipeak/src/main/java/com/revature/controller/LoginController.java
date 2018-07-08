ceb8332ae072c62be58f505f1298f71b29721566
Jacob Miller <millerjacob67@gmail.com>
2018-07-08 14:00:21 -0400

    Fixed the server issues. Not fully completed.

--- java/Recipeak/src/main/java/com/revature/controller/LoginController.java
+++ java/Recipeak/src/main/java/com/revature/controller/LoginController.java
@@ -2,9 +2,9 @@ package com.revature.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
